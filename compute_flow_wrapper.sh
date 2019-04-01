f6e804f721c1f8f797139db3b96821aadb854296
Will Price <will.price94@gmail.com>
2019-04-01 21:36:07 +0100

    Fix broken wrapper script
    `compute_flow` can't deal with writing absolute paths, so we have to
    use `$OF_PATTERN` instead of `$OUT/$OF_PATTERN`.

--- compute_flow_wrapper.sh
+++ compute_flow_wrapper.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
+shopt -s nullglob
+set -e
 
-: ${COMPUTE_FLOW:=/bin/compute_flow}
+COMPUTE_FLOW=/bin/compute_flow 
 
 if [[ $# -lt 1 || $1 = '-h' || $1 = '--help' ]]; then
     echo "USAGE: <frame_pattern> [<compute_flow_arg>]+"
     echo "Example: $0 frame%06d.jpg"
-    echo
+    echo 
+    echo 
     "$COMPUTE_FLOW" --help
     exit 1
 fi
@@ -13,12 +16,25 @@ fi
 IN=/input
 OUT=/output
 
-FRAME_PATTERN="$1"; shift
+FRAME_PATTERN="${1:-'frame_%06d.jpg'}"; shift
 OF_PATTERN="flow_%s_$FRAME_PATTERN"
 
 mkdir -p "$OUT/"{u,v}
 
-"$COMPUTE_FLOW" "$IN" "$FRAME_PATTERN" "$OUT/$OF_PATTERN" $@
+touch "$IN/.flow" || exit 1
+rm "$IN/.flow" || exit 1
 
-mv "$OUT/flow_x*.jpg" "$OUT/u"
-mv "$OUT/flow_y*.jpg" "$OUT/v"
+"$COMPUTE_FLOW" "$IN" "$FRAME_PATTERN" "$OF_PATTERN" $@
+
+for dim in x y; do
+    for f in "$IN/flow_${dim}_"*.jpg; do
+        mv "$f" "$OUT/u/"
+    done
+done
+
+shopt -u nullglob
+for dir in u v; do
+    pushd "$OUT/$dir" >/dev/null
+    rename 's/flow_._//g' *
+    popd >/dev/null
+done
